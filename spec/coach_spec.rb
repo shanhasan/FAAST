2253b2bac3ad16277efdb20465a5c7d2f8443482
Shan Hasan <shan119@hotmail.co.uk>
2014-11-11 07:52:33 +0000

    added Coach files

---
+++ spec/coach_spec.rb
@@ -0,0 +1,14 @@
+require 'coach'
+
+describe Coach do
+  
+end
+
+#Tests
+#Let passengers enter coach
+#Let passengers exit coach
+#Has a limit
+#No passengers can enter after linit
+#Passenger count can't be negative
+#passengers can only enter when train is at a station
+#passengers can only exit when train is at a station
