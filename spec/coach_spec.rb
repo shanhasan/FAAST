fdd964a459fc570324c533a1ecb7fc57cb5059f5
Shan Hasan <shan119@hotmail.co.uk>
2014-11-11 14:23:07 +0000

    updated files

--- spec/coach_spec.rb
+++ spec/coach_spec.rb
@@ -1,6 +1,27 @@
 require 'coach'
 
 describe Coach do
+
+  it "lets passengers enter coach" do
+  end
+
+  it "lets passengers exit coach" do
+  end
+
+  it "Has a maximum capacity" do
+  end
+
+  it "Capacity can't be negative" do
+  end
+
+  it "Passenger count can't be negative" do
+  end
+
+  it "passenger can only enter when train is at station" do
+  end
+
+  it "passengers can only exit when train is at station" do
+  end
   
 end
 
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -1,7 +1,19 @@
 require "train"
 
 describe Train do
+
+  it "can travel between stations" do
+  end
   
+  it "is made up of coaches" do
+  end
+
+  it "has a minimum limit of number of coaches" do
+  end
+
+  it "has a maximum limit of number of coahces" do
+  end
+
 end
 
 #Tests
