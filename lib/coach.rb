f62fc09e9fe704749d819717a073d74920e763d6
Shan Hasan <shan119@hotmail.co.uk>
2014-11-18 11:47:18 +0000

    updated coach files

--- lib/coach.rb
+++ lib/coach.rb
@@ -1,15 +1,27 @@
-class Coach
+require 'train'
+
+class Coach < Train
 
   attr_accessor :capacity_passengers
 
   DEFAULT_CAPACITY_PASSENGERS = 40
 
-  def initialized (options= {})
+  def initialize(options= {})
     @capacity_passengers = options.fetch(:capacity_passengers, DEFAULT_CAPACITY_PASSENGERS)
   end
 
   def passengers #module
    @passenger ||=[]
+  end
+
+  def has_the_train_arrived?(passenger)
+    raise "train hasn't arrived yet" if @current_station != passenger.origin
+    board(passenger)
+  end
+
+  def has_the_train_reached?(passenger)
+    raise "please wait till your station" if @current_station != passenger.destination
+    disembark(passenger)
   end
 
   def board(passenger)
--- spec/coach_spec.rb
+++ spec/coach_spec.rb
@@ -1,29 +1,57 @@
 require 'coach'
+require 'train'
 
 describe Coach do
 
   let (:coach) {Coach.new}
+  let (:train) {Train.new}
   let (:passenger) {double :passenger}
+  let (:passenger1) {double :passenger, :origin => "bank", :destination => "old street"}
+  let (:passenger2) {double :passenger, :origin => "london bridge", :destination => "morgate"}
 
-  it "lets passengers enter coach" do
-    coach.board(passenger)
-    expect(coach.passenger_count).to eq(1)
-  end
+  context "Basic coach functions" do
 
-  it "lets passengers exit coach" do
-    coach.board(passenger)
-    coach.disembark(passenger)
-    expect(coach.passenger_count).to eq(0)
-  end
-
-  it "passengers can't enter when train is full" do
-    40.times{coach.board(passenger)}
-    expect{coach.board(passenger)}.to raise_error RuntimeError
+    it "lets passengers enter coach" do
+      coach.board(passenger)
+      expect(coach.passenger_count).to eq(1)
     end
 
-  it "passenger count can't be negative" do
-    expect(coach.passenger_count).to eq(0)
-    expect{coach.disembark(passenger)}.to raise_error RuntimeError
+    it "lets passengers exit coach" do
+      coach.board(passenger)
+      coach.disembark(passenger)
+      expect(coach.passenger_count).to eq(0)
+    end
+
+    it "passengers can't enter when train is full" do
+      40.times{coach.board(passenger)}
+      expect{coach.board(passenger)}.to raise_error RuntimeError
+      end
+
+    it "passenger count can't be negative" do
+      expect(coach.passenger_count).to eq(0)
+      expect{coach.disembark(passenger)}.to raise_error "There are no passengers on the train"
+    end
+  end
+
+  context "More advanced coach functions" do
+
+    it "should pick up passengers when at their origin" do
+      coach.current_station= 'bank'
+      coach.has_the_train_arrived?(passenger1)
+      expect(coach.passenger_count).to eq(1)
+    end
+ 
+    it "should Not pick up passengers when Not at their origin" do
+      coach.current_station= 'bank'
+      expect{coach.has_the_train_arrived?(passenger2)}.to raise_error "train hasn't arrived yet"
+    end
+
+    it "should release passengers when at their destination" do
+      coach.passengers == [passenger2]
+      coach.current_station= 'morgate'
+      coach.has_the_train_reached?(passenger2)
+      expect(coach.passenger_count).to eq(0)
+    end
   end
 end
 
